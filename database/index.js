9504a8d133763574ec784ce475bc4052878097ae
gaurangsh <60140780+gaurangsh@users.noreply.github.com>
2022-06-12 10:55:52 +0530

    Update index.js

--- database/index.js
+++ database/index.js
@@ -1,6 +1,6 @@
 module.exports.init = function(){
     const mongoose = require('mongoose');
-    mongoose.connect('mongodb+srv://app:1234567890@cluster0.rq2ix.mongodb.net/fasal_project?retryWrites=true&w=majority')
+    mongoose.connect('mongodb+srv://<username>:<password>@cluster0.rq2ix.mongodb.net/fasal_project?retryWrites=true&w=majority')
     .then(function(){
       console.log("db is on");
     }).catch(function(){
